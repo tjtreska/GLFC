c3e739dbb885a80f0c875d9e2d16ad49a84f23b8
Ted Treska <TTreska@glfc.org>
2024-06-18 14:49:35 -0400

    Adjusted LM target values
    Adjusted LM target values in variables years (to 2015-2019) and adjust (1).

--- R/AItarget.R
+++ R/AItarget.R
@@ -19,13 +19,13 @@
 #'   Huron did not have a time period with acceptable sea lamprey wounding
 #'   rates, the target was set to 25\% of the mean for 1989-1993.
 #' @details
-#'   The Lake Michigan Committee accepted the Sea Lamprey Control Board's 
-#'   recommendation for an Adult Index target at its Executive Session, 
-#'   22 March 2016.  Specifically, the years of acceptable wounding used to 
-#'   define the Adult Index target for Lake Michigan sea lampreys was 
-#'   changed to a time period with available Adult Index data, 1995-1999, and 
-#'   the index target was defined as the mean of the Adult Index during these 
-#'   years, adjusted by 5/8.9 to compensate for the highest wounding rate 
+#'   The Lake Michigan Committee accepted the Sea Lamprey Control Board's
+#'   recommendation for an Adult Index target at its Executive Session,
+#'   22 March 2016.  Specifically, the years of acceptable wounding used to
+#'   define the Adult Index target for Lake Michigan sea lampreys was
+#'   changed to a time period with available Adult Index data, 1995-1999, and
+#'   the index target was defined as the mean of the Adult Index during these
+#'   years, adjusted by 5/8.9 to compensate for the highest wounding rate
 #'   observed during this period.
 #' @return
 #'   A data frame with the calculated targets for the Adult Index of each
@@ -33,8 +33,8 @@
 #' @export
 
 AItarget <- function(lakeIndex,
-  years=list(1994:1998, 1995:1999, 1989:1993, 1991:1995, 1993:1997),
-  adjust=c(1, 5/8.9, 0.25, 1, 1)) {
+  years=list(1994:1998, 2015:2019, 1989:1993, 1991:1995, 1993:1997),
+  adjust=c(1, 1, 0.25, 1, 1)) {
 
   targ <- data.frame(lake=1:5, targInd=rep(NA, 5))
 
