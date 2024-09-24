4c040026c5c181f668297f6a50a3a4fb84d7aaba
Ted Treska <TTreska@glfc.org>
2024-09-24 15:38:02 -0400

    Update to Lake Michigan target, so it is based on real data.
    Replaced old version that used 5/8.9 conversion of widely varying marking rates with more accurate and recent data time series. Commented out previous time series along with 5/8.9 conversion. See Treska et al 2021

--- R/SRpresto.R
+++ R/SRpresto.R
@@ -250,8 +250,9 @@ SRpresto <- function(FOLDER, INDEX.LAKE, INDEX.STREAM, MAXLARVAE,
 
 
   #### Targets - this makes the StatusTargets.csv file ####
-  sptargyrz <- list(1994:1998, 1988:1992, 1989:1993, 1991:1995, 1993:1997)
-  sptargyrz <- list(1994:1998, 1995:1999, 1989:1993, 1991:1995, 1993:1997)
+  #sptargyrz <- list(1994:1998, 1988:1992, 1989:1993, 1991:1995, 1993:1997)
+  #sptargyrz <- list(1994:1998, 1995:1999, 1989:1993, 1991:1995, 1993:1997)  #previous time frames
+  sptargyrz <- list(1994:1998, 2015:2019, 1989:1993, 1991:1995, 1993:1997)  # this is adjustment for new Lake Michigan Target value, fall 2024
   TARGET <- data.frame(lake=1:5, wound.target=rep(c(5, 2), c(4, 1)),
     wound.units=rep(c("A1-A3", "A1"), c(4, 1)),
   	index.target=rep(NA, 5), intarg.lo=rep(NA, 5), intarg.hi=rep(NA, 5))
@@ -262,7 +263,7 @@ SRpresto <- function(FOLDER, INDEX.LAKE, INDEX.STREAM, MAXLARVAE,
   	n <- length(pick5)
   	ci <- qnorm(1 - 0.05/2) * sqrt(var(pick5)) / sqrt(n)
   	TARGET[i, 5:6] <- mean(pick5) + c(-1, 1)*ci	# using z dist (known variance)
-  	if (i==2) TARGET[i, 4:6] <- (5/8.9)*TARGET[i, 4:6]
+  	#if (i==2) TARGET[i, 4:6] <- (5/8.9)*TARGET[i, 4:6]   Since we are using real data for LM, no need to adjust anymore.
   	if (i==3) TARGET[i, 4:6] <- 0.25*TARGET[i, 4:6]
   	}
   targyrz <- apply(sapply(sptargyrz, range), 2, paste, collapse="-")
